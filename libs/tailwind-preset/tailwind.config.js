ac10444d8ff692fe4a028944a75fc8577bfeedc8
Maik Fischer <maikfischer22@gmail.com>
2022-06-24 17:36:54 +0200

    Add tailwind & shared library

---
+++ libs/tailwind-preset/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        xs: 'var(--spacing-xs)',
+        s: 'var(--spacing-s)',
+        m: 'var(--spacing-m)',
+        l: 'var(--spacing-l)',
+        xl: 'var(--spacing-xl)',
+      },
+    },
+  },
+  plugins: [],
+};
